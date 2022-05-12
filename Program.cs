6413eedd0b7550f06376292f6e7deb747005d927
NickoSanchez <105216750+NickoSanchez@users.noreply.github.com>
2022-05-12 11:33:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//    var no = Console.ReadLine();
+//}
+//else if (no == "N")
+//{
+//    Console.WriteLine("Programa Finalizado Correctamente");
+//}
+//else
+//{
+//    Console.WriteLine("Opcion No valida");
+//}
+
+//Console.Clear();
+//Console.WriteLine("Ingrese su nombre: ");
+//    var nombre = Console.ReadLine();
+//    Console.WriteLine("¡Hola " + nombre + "!");
+//    Console.WriteLine("¿Desea Continuar? S/N");
+//    var respuesta = Console.ReadLine();
+
+//if (respuesta.ToUpper() == "S")
+//{
+//    Console.WriteLine("Ingrese su nombre: ");
+//    var nombre2 = Console.ReadLine();
+//    Console.WriteLine("¡Hola " + nombre2 + "!");
+//    Console.WriteLine("¿Desea Continuar? S/N");
+//    var respuesta2 = Console.ReadLine();
+//}
+//else if (respuesta.ToUpper() == "N")
+//{
+//    Console.WriteLine("Programa Finalizado Correctamente");
+//}
+//else
+//{
+//    Console.WriteLine("Opcion no valida");
+//}
+
+Console.Clear();
+while (true)
+{
+    Console.WriteLine("Ingrese su nombre: ");
+    var nombre = Console.ReadLine();
+    Console.WriteLine("¡Hola " + nombre + "!");
+    Console.WriteLine("¿Desea Continuar? S/N");
+    var respuesta = Console.ReadLine();
+    if (respuesta.ToUpper() == "S")
+    {
+    }
+    else if (respuesta.ToUpper() == "N")
+    {
+        Console.WriteLine("Programa finalizado correctamente");
+        Environment.Exit(0);
+    }
+    else
+    {
+        Console.WriteLine("Opcion no valida");
+        Environment.Exit(0);
+    }
+}
